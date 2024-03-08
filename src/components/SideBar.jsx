67ca565c59f502e4eae24eb727cdd8fe86dc81f7
Achraf Kaou <achraf03achref@gmail.com>
2024-03-08 17:56:25 +0100

    dashboard0.2

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,21 +1,24 @@
 import { Card, Typography, List, ListItem } from "@material-tailwind/react";
-import logo from '../assets/logo.png';
+
 
 
 const SideBar = ({ links }) => {
   return (
-    <Card className="h-[calc(100vh-2rem)] bg-gray-50 w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
-      <List>
-        {links.map((link, index) => (
-          <ListItem className="text-primary-50 text-xl" key={index}>
-            {link.icon} {/* Render the icon */}
-            {link.label}
-          </ListItem>
-        ))}
-        <hr className="my-2 border-primary-50" />
-      </List>
-    </Card>
+    <div className="fixed top-0 left-0 h-full w-2/12 pt-20">
+      <Card className="h-full bg-gray-50 w-full p-4 shadow-xl shadow-blue-gray-900/5 pt-10">
+        <List>
+          {links.map((link, index) => (
+            <ListItem className="text-primary-50 text-xl shadow-sm shadow-primary-50" key={index}>
+              {link.icon} {/* Render the icon */}
+              {link.label}
+            </ListItem>
+          ))}
+        </List>
+      </Card>
+    </div>
   );
 }
+
+
 
 export default SideBar;
