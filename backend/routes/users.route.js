7d2dbf75f3e709c8d4dcf79e7c2eb93ca6e439ea
Zouaoui-Montassar <zouaouimonta12345@gmail.com>
2024-03-20 05:44:12 +0100

    zedt middlewares + satrin fel users route

--- backend/routes/users.route.js
+++ backend/routes/users.route.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const { userController } = require('../controllers/users.controller');
 const router = express.Router();
+const verifyToken = require('../middlewares/verifyToken');
 
 router.post('/users/signUp', userController.addUser);
 router.post('/users/login', userController.login);
 router.delete('/users/deleteUser', userController.deleteUser);
+
+/* 
+//Protected route
+router.get('/protected', verifyToken, userController.protectedEndpoint);  // lverification tsir bel jwt
+*/
 
 
 module.exports.userRouter = router;
