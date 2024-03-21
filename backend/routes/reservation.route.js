c2354bf8aaa81736b8705a48f2605a8796f8e733
Achraf Kaou <achraf03achref@gmail.com>
2024-03-21 08:37:49 +0100

    reservation backend 0.1

---
+++ backend/routes/reservation.route.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { reservationController } = require('../controllers/reservation.controller');
+const router = express.Router();
+
+router.post('/reservation/add/:partId', reservationController.addReservation);
+router.put('/reservation/update/:reservationId', reservationController.updateReservation);
+router.put('/reservation/annul/:reservationId', reservationController.annulerReservation);
+router.put('/reservation/termin/:reservationId', reservationController.terminerReservation);
+router.get('/reservation/search/:partId', reservationController.searchReservation);
+router.get('/reservation/listP/:partId', reservationController.listReservationP);
+router.get('/reservation/listR/:resId', reservationController.listReservationR);
+
+
+module.exports.reservationRouter = router;
+ 
