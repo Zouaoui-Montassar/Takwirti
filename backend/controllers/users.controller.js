191fcde4a1862aa3199180452d5cc4e173bbbeb6
Zouaoui-Montassar <zouaouimonta12345@gmail.com>
2024-03-21 06:01:58 +0100

    + get user by Id

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -120,6 +120,18 @@ const deleteUser = async (req, res) => {
     }
 };
 
+const getUserById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const user = await UserModel.findById(id);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.status(200).json({ user });
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error", error: error.message });
+    }
+};
 
 module.exports.userController = {
     addResp,
@@ -128,7 +140,8 @@ module.exports.userController = {
     updateParticulier,
     updateResponsable,
     getAllUsers,
-    deleteUser
+    deleteUser,
+    getUserById
 };
 
 
