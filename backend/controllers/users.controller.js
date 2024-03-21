48577cccea4350e4317d915275f369f0a9acb4db
Zouaoui-Montassar <zouaouimonta12345@gmail.com>
2024-03-21 03:06:23 +0100

    New login w register done, starting el updates

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -1,9 +1,103 @@
-const { userModel } = require('../models/user.model');
+const { ResponsableModel } = require('../models/responsable.model');
+const { ParticulierModel } = require('../models/particulier.model');
+const UserModel = require('../models/user.model');
 const bcrypt = require("bcryptjs");
-const jwt = require('jsonwebtoken')
-const jwtSecret = '4715aed3c946f7b0a38e6b534a9583628d84e96d10fbc04700770d572af3dce43625dd'
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const jwtSecret = process.env.JWT_SECRET;
 
-const addUser = async (req, res) => {
+
+const addResp = async (req, res) => {
+    const { email, password, num_tel, ...otherData } = req.body;
+    console.log(ResponsableModel);
+
+    try {
+        const existingUser = await ResponsableModel.findOne({ email: email });
+        if (existingUser) {
+            return res.status(400).json({
+                message: "User with this email already exists",
+            });
+        }
+        const hash = await bcrypt.hash(password, 10);
+        const newResp = await ResponsableModel.create({ email: email, password: hash, num_tel: num_tel, ...otherData });
+        res.status(200).json({
+            message: "Responsable successfully created",
+            user: newResp,
+        });
+    } catch (error) {
+        res.status(400).json({
+            message: "Responsable not successfully created",
+            error: error.message,
+        });
+    }
+};
+
+
+const addParticulier = async (req, res) => {
+    const { email, password, ListeAmi, ...otherData } = req.body;
+    try {
+        const existingUser = await ParticulierModel.findOne({ email: email });
+        if (existingUser) {
+            return res.status(400).json({
+                message: "User with this email already exists",
+            });
+        }
+        const hash = await bcrypt.hash(password, 10);
+        const newParticulier = await ParticulierModel.create({ email: email, password: hash, ListeAmi: ListeAmi, ...otherData });
+        res.status(200).json({
+            message: "Particulier successfully created",
+            user: newParticulier,
+        });
+    } catch (error) {
+        res.status(400).json({
+            message: "Particulier not successfully created",
+            error: error.message,
+        });
+    }
+};
+
+
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await UserModel.findOne({ email: email });
+        if (!user) {
+            return res.status(401).json({ message: "User not found" });
+        }
+        const validPassword = await bcrypt.compare(password, user.password);
+        if (!validPassword) {
+            return res.status(401).json({ message: "Invalid password" });
+        }
+        const token = jwt.sign({ userId: user._id, email: user.email }, jwtSecret);
+        res.status(200).json({ message: "Login successful", token: token });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+
+
+
+module.exports.userController = {
+    addResp,
+    addParticulier,
+    login
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const addUser = async (req, res) => {
     const { email, password } = req.body;
     try {
         const existingUser = await userModel.findOne({ email: email });
@@ -83,3 +177,4 @@ module.exports.userController = {
     login,
     deleteUser,
 };
+ */
--- backend/models/responsable.model.js
+++ backend/models/responsable.model.js
@@ -1,8 +1,12 @@
 const { Schema } = require("mongoose");
+const User = require('./user.model');
 
 const ResponsableSchema = new Schema({
     numTel: { type: String, required: true }
 });
+const ResponsableModel = User.discriminator('Responsable', ResponsableSchema);
 
-module.exports = ResponsableSchema;
+module.exports = { ResponsableModel };
+
+
 
