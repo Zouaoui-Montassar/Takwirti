f3b929487550f51e3f508c086ac7b909cb7db77e
Zouaoui-Montassar <zouaouimonta12345@gmail.com>
2024-03-26 06:13:35 +0100

    updated schemas ( profile pic mazelt )

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -7,8 +7,7 @@ const UserSchema = new Schema({
     email: { type: String, required: true , unique : true },
     password: { type: String, required: true },
     DN: { type: Date, required: true },
-    role: { type: String, required: true},
-    tel: { type: String, required: true },
+    tel: { type: String, required: true }
 });
 
 
