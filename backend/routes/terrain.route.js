e69ab534562f06305648d7f78c0b42262339ce37
Achraf Kaou <achraf03achref@gmail.com>
2024-03-27 17:57:56 +0100

    terrain responsable

--- backend/routes/terrain.route.js
+++ backend/routes/terrain.route.js
@@ -7,7 +7,7 @@ router.put('/terrain/update/:terrainId', terrainController.updateTerrain);
 router.delete('/terrain/delete/:terrainId', terrainController.deleteTerrain);
 router.get(`/terrain/search`, terrainController.searchTerrain);
 router.get('/terrain/list/:respId', terrainController.listTerrain);
-router.post('/terrain/modifcalendar/:respId',terrainController.updateCalendar);
+//router.post('/terrain/modifcalendar/:respId',terrainController.updateCalendar);
 router.get('/terrain/get', terrainController.getTerrain);
 
 module.exports.terrainRouter = router;
