47094576e56fa96d51311c5222346ac248a2ef3a
Zouaoui-Montassar <zouaouimonta12345@gmail.com>
2024-03-30 13:37:59 +0100

    Removed some unnecessary code

--- backend/models/particulier.model.js
+++ backend/models/particulier.model.js
@@ -8,19 +8,3 @@ const ParticulierModel = User.discriminator('Particulier', ParticulierSchema);
 
 module.exports = { ParticulierModel };
 
-
-
-
-
-
-// hedhi eli temchi cv , lfou9 bech njareb nrod el friend list array ObjectId
-/* const { Schema } = require("mongoose");
-const User = require('./user.model');
-const ParticulierSchema = new Schema({
-    ListeAmi: { type: String, required: true }
-});
-
-const ParticulierModel = User.discriminator('Particulier', ParticulierSchema);
-
-module.exports = { ParticulierModel };
- */
