9b1e430c2bc6acd6342c6b1405c0a9ea24200b01
raefka <raefkacem20@gmail.com>
2024-03-31 12:31:36 +0100

    erreur

--- frontend/src/components/ReservationList.jsx
+++ frontend/src/components/ReservationList.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import SearchBox from './SearchBox';
 
@@ -21,7 +22,39 @@ const reservation = [
   },*/
 ];
 
-const ReservationList = () => {
+const ReservationList = ({xxx,id}) => {
+
+    const [reservations, setReservations] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                if(xxx === "Particulier"){
+                // Make an HTTP GET request to fetch reservations from the backend
+                  const response = await axios.get(`http://localhost:4000/ter/reservation/listP/${id}`);
+                // Assuming the backend returns data in the format { reservations: [...] }
+                  setReservations(response.data.reservations);
+                  console.log(response);
+                  console.log(response.data)
+                  console.log(response.data.reservations);
+                
+                }else if(xxx === "responsable"){
+                   // Make an HTTP GET request to fetch reservations from the backend
+                    const response = await axios.get(`http://localhost:4000/ter/reservation/listR/${id}`);
+                    // Assuming the backend returns data in the format { reservations: [...] }
+                    setReservations(response.data.reservations);
+                    console.log(response);
+                    console.log(response.data)
+                    console.log(response.data.reservations);
+                }
+            } catch (error) {
+                console.error('Error fetching reservations:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     const navigate = useNavigate();
     function toReservationEdit (idterrain){
         navigate(`/reservation/edit/:${idterrain}`);
@@ -37,13 +70,13 @@ const ReservationList = () => {
                 </div>
 
                     <div className="w-full h-full overflow-auto shadow bg-white" id="journal-scroll">
-                        {reservation.length > 0 ? (
+                        {reservations.length > 0  ? (
                             <table className="w-full">
                                 <tbody>
-                                    {reservation.map((item, index) => (
+                                    {reservations.map((item, index) => (
                                         <tr key={index} onClick={() => toReservationEdit(item.idterrain)} className="relative transform scale-10 text-xs py-1 border-b-2 border-blue-100 cursor-default bg-blue-500 bg-opacity-25">
                                             <td className="pl-5 pr-3 whitespace-no-wrap">
-                                                <div className="text-gray-400">{item.Date}</div>
+                                                <div className="text-gray-400">{item.date}</div>
                                                 <div>{item.time}</div>
                                             </td>
                                             <td className="px-2 py-2 whitespace-no-wrap">
