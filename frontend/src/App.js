e03e4978af0aa2a3a03690710aea0f4556862ad4
raefka <raefkacem20@gmail.com>
2024-04-04 15:42:57 +0100

    responsable page na9sa mtaa reservation w herure bloqué

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,6 +27,7 @@ import Notifications from './components/Notifications';
 import Tachkila from './components/Tachkila';
 import { TeamProvider } from './context/Teamcontext';
 import ReservationAddParent from './components/ReservationAddParent';
+import Responsable2 from './components/Responsable2';
 
 const data = [
   { id: 1, name: 'Item 1', image:'/Section 1 image.jpg' },
@@ -64,7 +65,7 @@ function App() {
           <Route path="/signin" element={<Sign_in/>} />
           <Route path="/stats" element={<Stats/>} />
           <Route path='/responsable' element={<Responsable/>}/>  {/* protected */} 
-          <Route path='/responsable/:id' element={<Responsable/>}/>
+          <Route path='/responsable/:terrainId' element={<Responsable2/>}/>
           <Route path='/terrain/add' element={<Terrain func={"add"} id={"9876543210fedcba"}/>}/>
           <Route path='/terrain/responsable' element={<TerrainsResp/>}/>
           <Route path='/particulier' element={<PageUtilisateur/>}/> {/* protected */} 
--- frontend/src/components/ParentCalender2.jsx
+++ frontend/src/components/ParentCalender2.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Calendar2 from './Calender2';
 import List from './List';
 
-const ParentComponent2 = () => {
+const ParentComponent2 = ({openTime,closeTime,time,date,rh}) => {
   const [selectedDate, setSelectedDate] = useState(new Date());
 
   const handleDateSelect = (date) => {
@@ -11,7 +11,7 @@ const ParentComponent2 = () => {
 
   return (
     <div >
-      <Calendar2 onDateSelect={handleDateSelect} />
+      <Calendar2 onDateSelect={handleDateSelect} dayBlocked={date} ouvre={openTime} ferme={closeTime} duree={time} hr={rh} />
     </div>
   );
 };
