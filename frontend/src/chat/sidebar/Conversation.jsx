266ae8974852b189dcc6c1fc86b4047ca6b43c42
Zouaoui-Montassar <zouaouimonta12345@gmail.com>
2024-04-11 08:52:38 +0100

    Offline status + green on hover

--- frontend/src/chat/sidebar/Conversation.jsx
+++ frontend/src/chat/sidebar/Conversation.jsx
@@ -10,12 +10,12 @@ const Conversation = ({conversation,lastIdx}) => {
 	return (
 		<>
 			<div
-				className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
-				${isSelected ? "bg-sky-500" : ""}
+				className={`flex gap-2 items-center hover:bg-green-500 rounded p-2 py-1 cursor-pointer
+				${isSelected ? "bg-green-500" : ""}
 			`}
 				onClick={() => setSelectedConversation(conversation)}
 			>
-				<div className={`avatar ${isOnline ? "online" : ""}`}>
+				<div className={`avatar ${isOnline ? "online" : "offline"}`}>
 					<div className='w-12 rounded-full'>
 						<img
 							src={conversation.image}
