61d18d96f81916f01bfccaabeada6c0264b07957
Zouaoui-Montassar <zouaouimonta12345@gmail.com>
2024-04-11 15:20:35 +0100

    Improved notifications

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -8,7 +8,7 @@ const GetAllNotifUser  = async (req, res) => {
 
         const notifications = await NotifModel.find({ receiver: id }).populate({
             path: 'sender',
-            select: 'nom prenom'
+            select: 'nom prenom image'
         }).sort({ createdAt: -1 }); // tri bel latest , populate bech tjibli el nom wel prenom mel id mtaa el sender
         
         res.status(200).json({
